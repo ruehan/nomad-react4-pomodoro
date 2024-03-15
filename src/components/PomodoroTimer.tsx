f92a284b4ca33baeeb1e0b1d5222a2dfe0596810
vendredl <node.js_@kakao.com>
2024-03-16 00:50:37 +0900

    :speech_balloon: 타이머 기본 메커니즘 추가

--- src/components/PomodoroTimer.tsx
+++ src/components/PomodoroTimer.tsx
@@ -8,15 +8,33 @@ const PomodoroTimer: React.FC = () => {
 	const [goal, setGoal] = useRecoilState(goalState);
 	const [isActive, setIsActive] = useState(false);
 
-	useEffect(() => {}, [
-		isActive,
-		time,
-		round,
-		goal,
-		setTime,
-		setRound,
-		setGoal,
-	]);
+	useEffect(() => {
+		let interval: number | null = null;
+
+		if (isActive && time > 0) {
+			interval = window.setInterval(() => {
+				setTime((time) => time - 1);
+			}, 1000);
+		} else if (time === 0) {
+			const newRound = round === 4 ? 1 : round + 1;
+			const newGoal = round === 4 ? goal + 1 : goal;
+
+			setRound(newRound);
+			setGoal(newGoal);
+			setTime(25 * 60);
+
+			if (newGoal === 12) {
+				setIsActive(false);
+				alert("Congratulations! You've completed 12 goals.");
+			}
+
+			if (interval !== null) window.clearInterval(interval);
+		}
+
+		return () => {
+			if (interval !== null) window.clearInterval(interval);
+		};
+	}, [isActive, time, round, goal, setTime, setRound, setGoal]);
 
 	return null;
 };
