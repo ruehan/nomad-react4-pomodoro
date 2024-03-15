15f57c6c62181fd5a4ef9c770dae165d8b1d026d
vendredl <node.js_@kakao.com>
2024-03-16 00:37:27 +0900

    :speech_balloon: State 추가

---
+++ src/state/timerState.ts
@@ -0,0 +1,16 @@
+import { atom } from "recoil";
+
+export const timeState = atom({
+	key: "timeState",
+	default: 25 * 60,
+});
+
+export const roundState = atom({
+	key: "roundState",
+	default: 1,
+});
+
+export const goalState = atom({
+	key: "goalState",
+	default: 0,
+});
