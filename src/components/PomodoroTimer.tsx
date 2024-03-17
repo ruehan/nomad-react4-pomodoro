ee07a9ae6757c0418562855792b8875fb3dba50b
vendredl <node.js_@kakao.com>
2024-03-17 17:28:51 +0900

    :lipstick: Title 추가 및 스타일 변경

--- src/components/PomodoroTimer.tsx
+++ src/components/PomodoroTimer.tsx
@@ -6,9 +6,30 @@ import { styled } from "styled-components";
 import TimeCard from "./TimeCard";
 import { motion } from "framer-motion";
 
-const Container = styled.div`
+interface TimeBarProps {
+	isActive: boolean;
+}
+
+const Content = styled.div`
 	width: 100%;
 	height: 100vh;
+	display: flex;
+	flex-direction: column;
+`;
+
+const Title = styled.div`
+	background-color: tomato;
+	color: white;
+	font-size: 60px;
+	position: absolute;
+	top: 50px;
+	left: 50%;
+	transform: translateX(-50%);
+`;
+
+const Container = styled.div`
+	width: 100%;
+	height: 100%;
 	background-color: tomato;
 	display: grid;
 	grid-template-columns: 3fr 1fr;
@@ -18,9 +39,9 @@ const Container = styled.div`
 `;
 
 const CardContainer = styled.div`
-	width: 70%;
+	max-width: 70%;
+	min-width: 40%;
 	height: 70%;
-	/* background: white; */
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
 	grid-template-rows: repeat(3, 1fr);
@@ -31,9 +52,6 @@ const CardContainer = styled.div`
 const Btn = styled(motion.button)`
 	width: 100px;
 	height: 100px;
-	/* position: fixed; */
-	/* left: 46%; */
-	/* bottom: 50px; */
 	border-radius: 100px;
 	justify-items: center;
 	align-items: center;
@@ -42,8 +60,30 @@ const Btn = styled(motion.button)`
 	background-color: rgba(0, 0, 0, 0.15);
 
 	svg {
-		width: 50%;
+		width: 70%;
 	}
+`;
+
+const ControlContainer = styled.div`
+	width: 90%;
+	height: 90%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Label = styled.label`
+	font-size: 24px;
+	margin-bottom: 10px;
+	color: white;
+`;
+
+const TimeBar = styled.input<TimeBarProps>`
+	width: 100%;
+	height: 20px;
+	color: black;
+	cursor: ${(props) => (!props.isActive ? "pointer" : "not-allowed")};
 `;
 
 const PomodoroTimer: React.FC = () => {
@@ -67,10 +107,9 @@ const PomodoroTimer: React.FC = () => {
 			setRound(newRound);
 			setGoal(newGoal);
 			setTime(totalTime);
-			// setTime(60);
 			setIsActive(false);
 			if (newGoal === 12) {
-				alert("Congratulations! You've completed 12 goals.");
+				alert("완주..!");
 			}
 
 			if (interval !== null) window.clearInterval(interval);
@@ -88,68 +127,71 @@ const PomodoroTimer: React.FC = () => {
 	const minutes = Math.floor(time / 60);
 	const seconds = time % 60;
 
-	const handleSliderChange = (event) => {
-		const minutes = event.target.value; // 슬라이더로부터 분 단위의 값 받아오기
-		setTime(minutes * 60); // 분을 초로 변환하여 상태 업데이트
+	const handleSliderChange = (event: { target: { value: any } }) => {
+		const minutes = event.target.value;
+		setTime(minutes * 60);
 		setTotalTime(minutes * 60);
 	};
 
 	return (
-		<Container>
-			<CardContainer>
-				<TimeCard label="Minutes" value={minutes} />
-				<TimeCard label="Seconds" value={seconds} />
-				<TimeCard label="Round" value={round} />
-				<TimeCard label="Goal" value={goal} />
-				<Btn
-					onClick={toggleTimer}
-					whileHover={{
-						scale: 1.2,
-					}}
-					whileTap={{
-						scale: 0.8,
-					}}
-				>
-					{isActive ? (
-						<svg
-							dataSlot="icon"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-							aria-hidden="true"
-						>
-							<path
-								clipRule="evenodd"
-								fillRule="evenodd"
-								d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm5-2.25A.75.75 0 0 1 7.75 7h.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-.75.75h-.5a.75.75 0 0 1-.75-.75v-4.5Zm4 0a.75.75 0 0 1 .75-.75h.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-.75.75h-.5a.75.75 0 0 1-.75-.75v-4.5Z"
-							/>
-						</svg>
-					) : (
-						<svg
-							dataSlot="icon"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-							aria-hidden="true"
-						>
-							<path
-								clipRule="evenodd"
-								fillRule="evenodd"
-								d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Zm6.39-2.908a.75.75 0 0 1 .766.027l3.5 2.25a.75.75 0 0 1 0 1.262l-3.5 2.25A.75.75 0 0 1 8 12.25v-4.5a.75.75 0 0 1 .39-.658Z"
-							/>
-						</svg>
-					)}
-				</Btn>
-				<input
-					type="range"
-					min="1"
-					max="60"
-					value={time / 60}
-					onChange={handleSliderChange}
-				/>
-			</CardContainer>
-			<Sandglass currentTime={time} totalTime={totalTime} />
-		</Container>
+		<Content>
+			<Title>Pomodoro</Title>
+			<Container>
+				<CardContainer>
+					<TimeCard label="Minutes" value={minutes} />
+					<TimeCard label="Seconds" value={seconds} />
+					<TimeCard label="Round" value={round} />
+					<TimeCard label="Goal" value={goal} />
+					<Btn
+						onClick={toggleTimer}
+						whileHover={{
+							scale: 1.2,
+						}}
+						whileTap={{
+							scale: 0.8,
+						}}
+					>
+						{isActive ? (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+							>
+								<path
+									fillRule="evenodd"
+									d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						) : (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+							>
+								<path
+									fillRule="evenodd"
+									d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+									clipRule="evenodd"
+								/>
+							</svg>
+						)}
+					</Btn>
+					<ControlContainer>
+						<Label>Timer Setting</Label>
+						<TimeBar
+							isActive={isActive}
+							type="range"
+							min="1"
+							max="60"
+							value={totalTime / 60}
+							onChange={handleSliderChange}
+						/>
+					</ControlContainer>
+				</CardContainer>
+				<Sandglass currentTime={time} totalTime={totalTime} />
+			</Container>
+		</Content>
 	);
 };
 
