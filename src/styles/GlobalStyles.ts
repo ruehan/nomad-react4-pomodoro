fbaba33bb0a6fc370f4a97336072a24e34f61f2e
vendredl <node.js_@kakao.com>
2024-03-17 17:29:44 +0900

    :lipstick: Global Style 변경

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -9,6 +9,8 @@ const GlobalStyles = createGlobalStyle`
       sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    width: 100%;
+    height: 100vh;
   }
 `;
 
