274f6b15bcdfc94b10050fec2e0e0c442e351eb5
vendredl <node.js_@kakao.com>
2024-03-17 17:03:57 +0900

    :lipstick: Global Style 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { RecoilRoot } from "recoil";
+import GlobalStyles from "./styles/GlobalStyles.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<RecoilRoot>
+			<GlobalStyles />
 			<App />
 		</RecoilRoot>
 	</React.StrictMode>
